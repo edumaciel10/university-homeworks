e6495b1a8bfe90fd9d462d0ee3dff50a3a4a0a07
Eduardo Maciel de Matos <mrgamex49@gmail.com>
2021-09-23 01:40:56 -0300

    feat(card): create card model

---
+++ 2sem/atr3-alg1/Card.h
@@ -0,0 +1,15 @@
+#ifndef CARD_H
+#define CARD_H
+
+#define TAM 52
+#define ERRO (-32000)
+
+typedef struct card_st CARD;
+
+CARD *createCard (int key, char *suit, char *number);
+int deleteCard(CARD **card);
+void printCard(const CARD *card);
+int getCardNumber(const CARD *card);
+void item_imprimir(const CARD *item);
+
+#endif //CARD_H
